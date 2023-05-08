939090695ca9a9a4bd86df3b9643c7ae91160295
Almaz Galimov <almazpython@gmail.com>
2023-05-08 12:28:51 +0500

    add checks count in report

--- main.py
+++ main.py
@@ -13,32 +13,42 @@ def main():
     for shift in shifts_api:
         shop_index = shift['shop_index']
         sum_by_checks = shift['sum_by_checks']
+        checks_count = shift['checks_count']
         if shop_index in sum_by_shop:
-            sum_by_shop[shop_index] += sum_by_checks
+            sum_by_shop[shop_index]['sum_by_checks'] += sum_by_checks
+            sum_by_shop[shop_index]['sum_by_checks'] += checks_count
         else:
-            sum_by_shop[shop_index] = sum_by_checks
+            sum_by_shop[shop_index] = {'sum_by_checks': sum_by_checks, 'checks_count': checks_count}
 
     for shop_index, revenue in sum_by_shop.items():
-        message = f'Магазин {se[shop_index]} : {revenue:,.2f}'.replace(',', ' ')
+        message = f'Отчет за сегодня {se[shop_index]}:\n' \
+                  f'Чеки: {revenue["checks_count"]}\n' \
+                  f'Оборот: {revenue["sum_by_checks"]:,.2f} '.replace(',', ' ')
         print(message)
-        telegram_api.send_message(message)
+        # telegram_api.send_message(message)
 
         if shop_index in unclosed_shifts().keys():
             unclosed = unclosed_shifts()[shop_index]
             message = f'{se[shop_index]} \n не закрыта смена на кассе номер {" и ".join(map(str, unclosed))}'
-            telegram_api.send_message(message)
+            # telegram_api.send_message(message)
             print(message)
 
-    total = sum(sum_by_shop.values())
-    if not unclosed_shifts():
-        telegram_api.send_message(f'Во всех магазинах смены закрыты')
+    total_sum = 0
+    total_count = 0
 
+    for shop, values in sum_by_shop.items():
+        total_sum += values['sum_by_checks']
+        total_count += values['checks_count']
 
-    message = f'Выручка по магазинам за сегодня:\n {total:,.2f}'.replace(',', ' ')
+    # if not unclosed_shifts():
+        # telegram_api.send_message(f'Во всех магазинах смены закрыты')
+
+    message = f'Суммарный отчет за сегодня:\n' \
+              f'Чеки: {round(total_count)}\n' \
+              f'Оборот: {total_sum:,.2f}'.replace(',', ' ')
     print(message)
-    # for store, (revenue, checks_count) in revenue_by_store.items():
-    #     message += f"{store}: {revenue} руб. ({checks_count} чеков)\n"
-    telegram_api.send_message(message)
+    # telegram_api.send_message(message)
+
     for shop_cash, this_shift in near_shifts(get_shifts()).items():
         shop, cash = shop_cash
         shifts = this_shift
@@ -49,7 +59,7 @@ def main():
             nears += (m,)
         message = f'{se[shop]}, касса номер {cash} близкие номера смен:\n' \
                   f'{" и ".join(nears)}\nрекомендуется переоткрыть смены на основной фирме'
-        telegram_api.send_message(message)
+        # telegram_api.send_message(message)
         print(message)
 
 
